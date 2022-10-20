b96b36fbfb4856f7fd19df3f75ef2252da9b6e2a
李春涛 <2568723470@qq.com>
2022-10-20 13:30:28 +0800

    本地备份

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ module.exports = {
 	lintOnSave: false,
 	devServer: {
 		proxy: {
-			//测试  h 域名ttps://y4.wjw.cool/
-			//正式 h 正式ttps: //yujian02.xyz/
+			// 本地备份
 			// 和上面自定义的 baseURL 保持一致
 			'/api': {
 				target: 'https://y4.wjw.cool/',
